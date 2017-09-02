e7351569ac8cbfce251b27989481e07d5f25d3b5
Dominic Valenciana <kiricon@live.com>
2017-09-02 12:46:42 -0600

    Links are up and running.

--- fun-router.js
+++ fun-router.js
@@ -1,17 +1,4 @@
 /**
- * The template that is used for the shadow root for every copy of your element,
- * which houses the styles and layout for the element.
- */
-const template = document.createElement("template");
-template.innerHTML = `
-    <style>
-        :host {
-            display: block;
-        }
-    </style>
-`;
-
-/**
  * This is the class that controls each instance of your custom element.
  */
 class FunRouter extends HTMLElement {
@@ -28,10 +15,6 @@ class FunRouter extends HTMLElement {
 
     constructor() {
         super();
-
-        // create shadow root for any children context
-        this.attachShadow({mode: "open"});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
 
         // add any initial variables here
     }
@@ -68,3 +51,63 @@ class FunRouter extends HTMLElement {
 }
 
 customElements.define("fun-router", FunRouter);
+
+
+/**
+ * This is the class that controls each instance of your custom element.
+ */
+class FunLink extends HTMLElement {
+    /**
+     * Part of the custom element spec. Returns an array of strings that are 
+     * the names of attributes that this element observes/listens to.
+     * 
+     * @returns {Array} an array of strings, each of which representing an 
+     *  attribute.
+     */
+    static get observedAttributes() {
+        return [];
+    };
+
+    constructor() {
+        super();
+
+        // add any initial variables here
+    }
+
+    /**
+     * Part of the custom element spec. Called after your element is attached to
+     * the DOM. Do anything related to the element or its children here in most
+     * cases.
+     */
+    connectedCallback() {
+        this.addEventListener('click', this.click);
+    }
+
+    click() {
+        const href = this.getAttribute('href') || '';
+        history.pushState(null, '', href);
+    }
+
+    /**
+     * Part of the custom element spec. Called after your element is remove from
+     * the DOM. Disconnect any listeners or anything else here.
+     */
+    disconnectedCallback() {
+
+    }
+
+    /**
+     * Part of the custom element spec. Called when one of the observed
+     * attributes changes, either via setAttribute() or with the attribute being
+     * manually set in the HTML.
+     * 
+     * @param {String} name the name of the attribute that changed
+     * @param {Mixed} oldValue the previous value of the attribute
+     * @param {Mixed} newValue the new value of the attribute
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        // respond to a changed attribute here
+    }
+}
+
+customElements.define("fun-link", FunLink);
